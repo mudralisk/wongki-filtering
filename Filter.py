47d550f16cbd4ca3e9e11fa071cdd4560cd14489
mudralisk <lkh96lkh@gmail.com>
2019-10-14 03:53:14 +0900

    Create Filter.py

---
+++ Filter.py
@@ -0,0 +1,32 @@
+#-*- coding: utf-8 -*-
+import discord
+import random
+import os
+from random import randint
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '.')
+channel = client.get_channel("channel id")
+
+filter_test = ["tlqkf","whssk"]
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game('이쁜말 쓰는중'))
+    print('Bot is ready.')
+
+
+@client.event
+async def on_message(message):
+	if '시발' in message.content:
+		newmsg = message.content[:]
+		newmsg = newmsg.replace("시발","이런")
+		username = message.author.name
+		outputmsg = username +": "+ newmsg
+		await message.delete()
+		await message.channel.send(outputmsg)
+
+
+access_token = os.environ["BOT_TOKEN"]
+client.run(access_token)
+
